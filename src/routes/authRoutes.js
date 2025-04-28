dbaa788b607472da80423e2d8a51d22068215140
Michael Ildefonso <michaelildefonso8@gmail.com>
2025-04-27 19:21:23 -0700

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const passport = require("passport");
 const authController = require("../controllers/authController");
+const checkAdmin = require("../middleware/checkAdmin"); // Import checkAdmin middleware
 
 const router = express.Router();
 
-// MainApp user login route
+// User login route
 router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
 
 router.get(
@@ -14,6 +15,28 @@ router.get(
         try {
             const token = req.user.generateJwt();
             res.redirect(`${process.env.CLIENT_URL}/workspace?token=${token}`);
+        } catch (err) {
+            console.error("OAuth Error:", err);
+            res.status(500).json({ error: "Internal Server Error" });
+        }
+    }
+);
+
+// Admin login route
+router.get(
+    "/admin/google",
+    passport.authenticate("google-admin", { scope: ["profile", "email"] })
+);
+
+router.get(
+    "/admin/google/callback",
+    passport.authenticate("google-admin", { session: false, failureRedirect: "/" }),
+    checkAdmin, // Ensure the user is an admin
+    (req, res) => {
+        try {
+            console.log("Admin user:", req.user); // Debug log to verify req.user
+            const token = req.user.generateJwt();
+            res.redirect(`${process.env.ADMIN_URL}/home?token=${token}`);
         } catch (err) {
             console.error("OAuth Error:", err);
             res.status(500).json({ error: "Internal Server Error" });
