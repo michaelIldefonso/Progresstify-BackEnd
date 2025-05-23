6763c6430c51d249048c26eae29249057cad7665
Michael Ildefonso <michaelildefonso8@gmail.com>
2025-05-23 17:36:33 +0800

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,17 @@
 const { Pool } = require("pg");
 
-// Create a new pool instance
-// using the connection string from the environment variable
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
-});
-
+let pool;
+try {
+    // Create a new pool instance
+    // using the connection string from the environment variable
+    pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+    });
+    
+} catch (error) {
+    console.error("Error creating database pool:", error);
+    process.exit(1); // Exit the process with a failure code
+}
 
 module.exports = pool;
